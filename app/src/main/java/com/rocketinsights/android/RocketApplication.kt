a963c418c6d0174e591439e0b15d8a27c25d4333
Brian Manning <bri@brimanning.com>
2019-01-17 16:22:44 -0500

    Add commented out Crashlytics code

--- app/src/main/java/com/rocketinsights/android/RocketApplication.kt
+++ app/src/main/java/com/rocketinsights/android/RocketApplication.kt
@@ -23,5 +23,32 @@ class RocketApplication : Application() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        // uncomment this and the below CrashlyticsTree class out if using Fabric/Crashlytics
+//        else {
+//            Fabric.with(this, Crashlytics(), Answers())
+//            Timber.plant(CrashlyticsTree())
+//        }
     }
+
+//    private class CrashlyticsTree : Timber.Tree() {
+//        private val priorityKey = "priority"
+//        private val tagKey = "tag"
+//        private val messageKey = "message"
+//
+//        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+//            if (priority == Log.VERBOSE || priority == Log.DEBUG || priority == Log.INFO) {
+//                return
+//            }
+//
+//            Crashlytics.setInt(priorityKey, priority)
+//            Crashlytics.setString(tagKey, tag)
+//            Crashlytics.setString(messageKey, message)
+//
+//            if (t == null) {
+//                Crashlytics.logException(Exception(message))
+//            } else {
+//                Crashlytics.logException(t)
+//            }
+//        }
+//    }
 }
