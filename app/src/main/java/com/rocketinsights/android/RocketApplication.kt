dc44597dbbea2866e5133faebe7f9de86da7649c
Brian Manning <bri@brimanning.com>
2019-01-17 15:55:23 -0500

    Implement Koin

---
+++ app/src/main/java/com/rocketinsights/android/RocketApplication.kt
@@ -0,0 +1,18 @@
+package com.rocketinsights.android
+
+import android.app.Application
+import com.rocketinsights.android.viewmodels.MainViewModel
+import org.koin.android.ext.android.startKoin
+import org.koin.androidx.viewmodel.ext.koin.viewModel
+import org.koin.dsl.module.module
+import timber.log.Timber
+
+class RocketApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin(this, listOf(module {
+            viewModel { MainViewModel(this@RocketApplication) }
+        }))
+    }
+}
--- app/src/main/java/com/rocketinsights/android/ui/MainActivity.kt
+++ app/src/main/java/com/rocketinsights/android/ui/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.rocketinsights.android.ui
 
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.rocketinsights.android.R
 import com.rocketinsights.android.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        viewModel.message.observe(this, Observer { data ->
+        mainViewModel.message.observe(this, Observer { data ->
             message.text = data?.text
         })
     }
