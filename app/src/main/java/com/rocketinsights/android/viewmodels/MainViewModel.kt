9aa29fe0db14f7a0fa7f8fb6d2eaf88f82951180
Brian Manning <bri@brimanning.com>
2019-04-29 10:41:39 -0400

    Implement coroutines

--- app/src/main/java/com/rocketinsights/android/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/rocketinsights/android/viewmodels/MainViewModel.kt
@@ -3,17 +3,20 @@ package com.rocketinsights.android.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import android.os.Handler
 import com.rocketinsights.android.R
 import com.rocketinsights.android.models.Message
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     val message = MutableLiveData<Message>()
 
     init {
         message.postValue(Message(application.getString(R.string.loading)))
-        Handler().postDelayed({
+        GlobalScope.launch {
+            delay(5000)
             message.postValue(Message(application.getString(R.string.done)))
-        }, 5000)
+        }
     }
 }
