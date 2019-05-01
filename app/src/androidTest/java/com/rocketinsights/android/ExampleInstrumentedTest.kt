48dd37a04b5868ab687eb4b6fc5ecd108591bfef
Brian Manning <bri@brimanning.com>
2019-05-01 16:00:41 -0400

    Fix ktlint issue with wildcard import

--- app/src/androidTest/java/com/rocketinsights/android/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rocketinsights/android/ExampleInstrumentedTest.kt
@@ -2,11 +2,10 @@ package com.rocketinsights.android
 
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.assertEquals
 
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumentation test, which will execute on an Android device.
