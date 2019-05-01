0823982c8e3ac1bb4b6492e80f7d592c7a3c98c5
Brian Manning <bri@brimanning.com>
2019-05-01 15:27:57 -0400

    Add image animation transition

--- app/src/main/java/com/rocketinsights/android/ui/MainFragment.kt
+++ app/src/main/java/com/rocketinsights/android/ui/MainFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.rocketinsights.android.R
 import com.rocketinsights.android.viewmodels.MainViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -16,8 +18,11 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 class MainFragment : Fragment() {
     private val mainViewModel: MainViewModel by viewModel()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? =
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
             inflater.inflate(R.layout.fragment_main, container, false)
 
     override fun onResume() {
@@ -26,10 +31,20 @@ class MainFragment : Fragment() {
         (activity as? AppCompatActivity)?.supportActionBar?.title = "First Fragment"
         mainViewModel.message.observe(this, Observer { data ->
             message.text = data?.text
+            stockImage.visibility = View.VISIBLE
 
             message.setOnClickListener {
-                findNavController().navigate(R.id.action_mainFragment_to_secondFragment)
+                findNavController().navigate(R.id.actionSlideTransition)
             }
         })
+
+        Glide.with(this).load(R.drawable.stock_image).centerCrop().into(stockImage)
+
+        stockImage.setOnClickListener {
+            val extras = FragmentNavigatorExtras(
+                    stockImage to "stockImage"
+            )
+            findNavController().navigate(R.id.actionGrowTransition, null, null, extras)
+        }
     }
 }
--- app/src/main/java/com/rocketinsights/android/ui/SecondFragment.kt
+++ app/src/main/java/com/rocketinsights/android/ui/SecondFragment.kt
@@ -6,15 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.transition.TransitionInflater
+import com.bumptech.glide.Glide
 import com.rocketinsights.android.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class SecondFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? =
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? =
             inflater.inflate(R.layout.fragment_second, container, false)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedElementEnterTransition =
+                TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+    }
 
     override fun onResume() {
         super.onResume()
         (activity as? AppCompatActivity)?.supportActionBar?.title = "Second Fragment"
+
+        Glide.with(this).load(R.drawable.stock_image).centerCrop().into(stockImage)
     }
 }
