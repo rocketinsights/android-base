cd0bb34d7c1a9e492a2fc9223ab95201d28f3b40
Brian Manning <bri@brimanning.com>
2019-05-14 12:34:33 -0400

    Convert view model test to unit test

---
+++ app/src/test/java/com/rocketinsights/android/viewmodels/MainViewModelTest.kt
@@ -0,0 +1,49 @@
+package com.rocketinsights.android.viewmodels
+
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.rocketinsights.android.models.Message
+import org.junit.Assert
+import org.junit.Test
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.doReturn
+import com.rocketinsights.android.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Rule
+
+class MainViewModelTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    private val app = mock<Application> {
+        on { getString(R.string.loading) } doReturn "Loading…"
+    }
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+    }
+
+    @Test
+    fun getMessage() {
+        val viewModel = MainViewModel(app, mock())
+        Assert.assertNotNull(viewModel.message)
+        Assert.assertEquals(Message("Loading…"), viewModel.message.value)
+    }
+
+    @Test
+    fun getDelayedMessage() {
+        val viewModel = MainViewModel(app, mock {
+            on { getMessageAsync() } doReturn GlobalScope.async { Message("Done!") }
+        })
+
+        Thread.sleep(2010)
+        Assert.assertEquals(Message("Done!"), viewModel.message.value)
+    }
+}
