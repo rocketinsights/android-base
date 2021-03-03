b04b2b09bd817c893005fc9586f6f1980c222913
IvanToplak <ivan.toplak.dev@gmail.com>
2021-03-03 14:33:24 +0100

    Migrating from Kotlin synthetics to view binding.

---
+++ app/src/main/java/com/rocketinsights/android/extensions/ViewBindingExt.kt
@@ -0,0 +1,31 @@
+package com.rocketinsights.android.extensions
+
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
+
+/**
+ * Returns fragment binding property delegate, which may be used between onViewCreated and onDestroyView (inclusive).
+ */
+fun <T : ViewBinding> Fragment.viewBinding(bind: (View) -> T): ReadOnlyProperty<Fragment, T> =
+    object : ReadOnlyProperty<Fragment, T>, DefaultLifecycleObserver {
+        private var binding: T? = null
+
+        override fun getValue(thisRef: Fragment, property: KProperty<*>): T =
+            binding ?: bind(requireView()).also {
+                // cache it when the view isn't destroyed
+                if (viewLifecycleOwner.lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED)) {
+                    viewLifecycleOwner.lifecycle.addObserver(this)
+                    binding = it
+                }
+            }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            binding = null
+        }
+    }
