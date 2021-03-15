9efe8619e94478617fbc9e7ed44d7c27b2010057
Julian Falcionelli <julianfalcionelli@gmail.com>
2021-03-15 18:16:07 +0100

    Permissions Manager (#47)
    This implementation uses a library (https://github.com/QuickPermissions/QuickPermissions-Kotlin) that uses the old API to request permissions on Android. The new API for requesting permissions in Android is unfortunately very tied to the view, since callbacks have to be registered before the fragment's onCreate event is called.
    
    I wrapped the QuickPermissions library for use with Coroutines.
    
    I added the Kotlin extensions for Fragments and Activities in case we want to switch to the new API.

--- app/src/main/java/com/rocketinsights/android/ui/MainFragment.kt
+++ app/src/main/java/com/rocketinsights/android/ui/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.rocketinsights.android.ui
 
+import android.Manifest
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
@@ -12,13 +13,17 @@ import com.rocketinsights.android.R
 import com.rocketinsights.android.databinding.FragmentMainBinding
 import com.rocketinsights.android.extensions.getIOErrorMessage
 import com.rocketinsights.android.extensions.show
+import com.rocketinsights.android.extensions.showToast
 import com.rocketinsights.android.extensions.viewBinding
 import com.rocketinsights.android.viewmodels.MainFragmentMessage
 import com.rocketinsights.android.viewmodels.MainViewModel
+import com.rocketinsights.android.viewmodels.PermissionsResult
+import com.rocketinsights.android.viewmodels.PermissionsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment(R.layout.fragment_main) {
     private val mainViewModel: MainViewModel by viewModel()
+    private val permissionsViewModel: PermissionsViewModel by viewModel()
     private val binding by viewBinding(FragmentMainBinding::bind)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +41,13 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.messages_fragment -> item.onNavDestinationSelected(findNavController())
+            R.id.request_permissions -> {
+                permissionsViewModel.requestPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
@@ -61,6 +73,19 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
             binding.message.setOnClickListener {
                 findNavController().navigate(MainFragmentDirections.actionSlideTransition())
+            }
+        })
+
+        permissionsViewModel.permissionsResult.observe(viewLifecycleOwner, {
+            it.getContentIfNotHandled()?.let {
+                when (it) {
+                    is PermissionsResult.PermissionsGranted -> {
+                        requireContext().showToast(getString(R.string.permissions_granted))
+                    }
+                    is PermissionsResult.PermissionsError -> {
+                        requireContext().showToast(getString(R.string.permissions_denied))
+                    }
+                }
             }
         })
     }
