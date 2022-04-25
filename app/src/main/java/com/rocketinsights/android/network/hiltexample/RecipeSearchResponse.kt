5eb0a7495a005969befcb42e110028d30907293c
fedeturazzini <fede.turazzini@gmail.com>
2022-04-25 16:33:33 -0300

    code rev tomi

--- app/src/main/java/com/rocketinsights/android/network/hiltexample/RecipeSearchResponse.kt
+++ app/src/main/java/com/rocketinsights/android/network/hiltexample/RecipeSearchResponse.kt
@@ -3,6 +3,6 @@ package com.rocketinsights.android.network.hiltexample
 import com.rocketinsights.android.models.recipe_hilt.RecipeDto
 
 data class RecipeSearchResponse(
-    var count: Int,
-    var results: List<RecipeDto>,
+    val count: Int,
+    val results: List<RecipeDto>,
 )
