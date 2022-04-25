927c10ceb60f33e23e03a42e99ca3f55acd9f443
fedeturazzini <fede.turazzini@gmail.com>
2022-04-25 12:30:58 -0300

    cleaning imports

--- app/src/main/java/com/rocketinsights/android/ui/HiltActivity.kt
+++ app/src/main/java/com/rocketinsights/android/ui/HiltActivity.kt
@@ -1,19 +1,10 @@
 package com.rocketinsights.android.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.appcompat.app.AppCompatActivity
 import com.rocketinsights.android.viewmodels.HiltListViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @AndroidEntryPoint
 class HiltActivity : AppCompatActivity() {
