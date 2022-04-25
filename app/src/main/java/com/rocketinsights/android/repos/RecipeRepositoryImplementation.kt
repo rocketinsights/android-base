adfca453dea326035112875fa408289c36fae783
fedeturazzini <fede.turazzini@gmail.com>
2022-04-25 12:38:54 -0300

    code smell

--- app/src/main/java/com/rocketinsights/android/repos/RecipeRepositoryImplementation.kt
+++ app/src/main/java/com/rocketinsights/android/repos/RecipeRepositoryImplementation.kt
@@ -4,12 +4,12 @@ import com.rocketinsights.android.network.RecipeService
 import com.rocketinsights.android.network.models.recipe_hilt.Recipe
 import com.rocketinsights.android.network.models.recipe_hilt.RecipeDtoMapper
 
-class RecipeRepositoryImplementation (
+class RecipeRepositoryImplementation(
     private val recipeService: RecipeService,
     private val mapper: RecipeDtoMapper,
-): RecipeRepository {
+) : RecipeRepository {
 
-    override suspend fun search(token: String, page: Int, query: String): List<Recipe>? =
+    override suspend fun search(token: String, page: Int, query: String): List<Recipe> =
         mapper.toDomainList(recipeService.search(token = token, page = page, query = query).results)
 
     override suspend fun get(token: String, id: Int): Recipe =
